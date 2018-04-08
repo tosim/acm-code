1a2f1e65104e0f41bf1a0e0d7079a831a8595fc9
yyc <82415327@qq.com>
2018-04-08 23:09:38 +0800

    push something

--- test.cpp
+++ test.cpp
@@ -1,91 +1,61 @@
 #include <stdio.h>
-#include <algorithm>
-#include <stdlib.h>
+#include <math.h>
 
-using namespace std;
-
-const int maxn = 1005;
-
-struct Pix{
-	int pos;
-	int code;
+struct Point{
+	double x,y;
 };
+int dir[4][2] = {-1,0,1,0,0,-1,0,1};
 
-int cmp(Pix a,Pix b){
-	return a.pos < b.pos;
+int n;
+Point p[105];
+
+double dis(Point &a,Point &b){
+	return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
 }
 
-int width,tot,seg_num;
-int seg_map[maxn][2];
-Pix out_map[maxn*8];
-int out_num;
-
-int getVal(int pos){
-	int p = 0,i = 0;
-	while(p <= pos){
-		p += seg_map[i++][1];
+double sum(Point tp){
+	double ret = 0;
+	for(int i = 0;i < n;i++){
+		ret += dis(tp,p[i]);
 	}
-	return seg_map[i-1][0];
+	return ret;
 }
-int cal(int pos){
-	int row = pos / width;
-	int col = pos % width;
-	int Max = -1;
-	int val = getVal(pos);
-	for(int i = row-1;i <= row+1;i++){
-		for(int j = col-1;j <= col+1;j++){
-			int tpos = i*width + j;
-			if(i < 0 || j < 0 || j >= width || tpos >= tot || (i == row && j == col)) continue;//超出范围的不用计算
-			int tval = abs(getVal(tpos) - val);
-			if(tval > Max) Max = tval;				
-		}
-	}
-	return Max;
-}
+
 int main(){
-	while(scanf("%d",&width) && width != 0){
-		out_num = seg_num = tot = 0;
-		int tval,tlen;
-		while(scanf("%d%d",&tval,&tlen) && (tval || tlen)){
-			seg_map[seg_num][0] = tval;
-			seg_map[seg_num++][1] = tlen;
-			tot += tlen;
+	while(scanf("%d",&n) != EOF){
+		for(int i = 0;i < n;i++){
+			scanf("%lf%lf",&p[i].x,&p[i].y);
 		}
-		int pos = 0;
-		for(int i = 0;i < seg_num;i++){
-			//printf("pos = %d\n",pos);
-			int row = pos / width;
-			int col = pos % width;
-			for(int i = row-1;i <= row+1;i++){
-				for(int j = col-1;j <= col+1;j++){
-					int tpos = i*width + j;
-					if(i < 0 || j < 0 || j >= width || tpos >= tot) continue;//超出范围的不用计算
-					//printf("tpos = %d\n",tpos);
-					out_map[out_num].pos = tpos;
-					out_map[out_num++].code = cal(tpos);
+		int tcnt = 1;
+		Point s[5];
+		//s[0].x = s[0].y = 5000.0;
+		s[0].x = s[0].y = 0;
+		double Min = 0x7fffffff;
+		for(int i = 0;i < tcnt;i++){
+			Point st = s[i];
+			Point nt;
+
+			double step = 1000;
+			while(step > 0.1){
+				int flag = 1;
+				while(flag == 1){
+					flag = 0;
+					for(int k = 0;k < 4;k++){
+						nt.x = st.x + dir[k][0] * step;
+						nt.y = st.y + dir[k][1] * step;
+						double tSum = sum(nt);
+						if(tSum < Min){
+							Min = tSum;
+							st.x = nt.x;
+							st.y = nt.y;
+							flag = 1;
+						}	
+					}
 				}
+				step /= 2.0;
 			}
-			pos += seg_map[i][1];
-		}
-		//计算左下角的特殊情况
-		out_map[out_num].pos = tot - width;
-		out_map[out_num++].code = cal(tot-width);
-		
-		sort(out_map,out_map+out_num,cmp);
-	
-		printf("%d\n",width);
-		Pix pre = out_map[0];
-		for(int i = 1;i < out_num;i++){
-			if(out_map[i].code == pre.code) continue;
-			else{
-				printf("%d %d\n",pre.code,out_map[i].pos-pre.pos);
-				pre = out_map[i];
-			}
-		}
-		printf("%d %d\n",pre.code,tot - pre.pos);
-		printf("0 0\n");
+		}			
+		printf("%d\n",(int)(Min+0.5));
 	}	
-	printf("0\n");
 	return 0;
-	//clutter
 }
