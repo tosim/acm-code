9c27ccd884d9d13f617b080158b7924ee38639f3
tosim <82415327@qq.com>
2018-05-02 12:32:46 +0800

    do something

---
+++ cpp-stl-learning/lower_bound-upper_bound.md
@@ -0,0 +1,23 @@
+#STL 学习之lower_bound和upper_bound
+
+## 头文件
+在**< algorithm >**中包含lower_bound和upper_bound
+
+## 参数
+begin，end，key
+数组或不定长数组的起始和结束未知及要查询的key
+
+## 返回值
+返回一个迭代器，指向这个key
+
+## 在升序的容器中
+lower_bound 返回第一个 >= key 的元素的迭代器
+upper_bound 返回最后一个 <= key 的元素的***后一个元素***的迭代其
+
+## 在降序的容器中
+lower_bound 返回第一个 <= key 的元素的迭代器
+upper_bound 返回最后一个 >= key 的元素的***后一个元素***的迭代其
+
+## 理解
+也就是说，lower_bound和upper_bound在没找到等于key的元素时，返回的元素尽量往数组的中间靠
+
--- test/test.cpp
+++ test/test.cpp
@@ -1,133 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include <algorithm>
-#include <map>
+
 using namespace std;
+int a[] = {1,4,8,16,18};
 
-const int maxn = 5e5 + 1;
-const long long MOD = 1e9;
-
-long long sum[maxn][31];
-int a[maxn];
-map<long long,int> px;
-
-int n,m;
-void init(){
-    //memset(sum,0,sizeof(sum));//不需要初始化，而且写了就超时
-    px.clear();
-}
-
-int find_left(long long x){
-    int l = 0,r = n - 1;
-    while(l < r){
-        int m = (l + r) >> 1;
-        if(a[m] > x){
-            r = m;
-        }else if(a[m] < x){
-            l = m + 1;
-        }else{
-            l = m + 1;
-        }
-    }
-    if(a[l] > x){
-        return l;
-    }else{
-        return -1;
-    }
-}
-
-int find_right(long long x){
-    int l = 0,r = n - 1;
-    while(l + 1 < r){
-        int m = (l + r) >> 1;
-        if(a[m] > x){
-            r = m - 1;
-        }else if(a[m] < x){
-            l = m;
-        }else{
-            l = m;
-        }
-    }
-    if(a[r] <= x){
-        return r;
-    }else if(a[l] <= x){
-        return l;
-    }else{
-        return -1;
-    }
-}
-
-void test_find(){
-    n = 5;
-    a[0] = 2;
-    a[1] = 5;
-    a[2] = 10;
-    a[3] = 15;
-    a[4] = 16;
-    printf("l = %d , r = %d\n\n",find_left(16),find_right(18));
+void test(){
+    printf("%ld\n",lower_bound(a,a+5,1) - a);
+    printf("%ld\n",upper_bound(a,a+5,17) - a - 1);
 }
 int main(){
-    //test_find();
-    int T;
-    scanf("%d",&T);
-    while(T--){
-        init();
-        scanf("%d%d",&n,&m);
-        for(int i = 0;i < n;i++){
-            scanf("%d",&a[i]);
-        }
-        sort(a,a+n);
-        for(int i = 0;i < n;i++){
-            for(int k = 1;k <= 30;k++){
-                if(i == 0){
-                    sum[i][k] = a[i]/k;
-                }else{
-                    sum[i][k] = sum[i-1][k] + a[i] / k;
-                }
-            }
-        }
-        //print_sumik();
-        int p;
-        long long ans = 0;
-        for(int i = 1;i <= m;i++){
-            scanf("%d",&p);
-            //printf("cal p = %d\n",p);
-            if(px.find(p) != px.end()){
-                //printf("p = %d has been caled\n",p);
-                ans = (ans + (long long)i*px[p]%MOD)%MOD;
-                continue;
-            }
-            long long pk = p;
-            long long eans = 0;
-            //long long pre = pk;//这里特别注意，因为pk乘上去会超longlong，必须用pre记录pk的前一个合法值
-            //因为当p^k-1大于1e9的时候，也就是条件不成立的时候,p^k可能大于1e18
-            for(int k = 1;/*pre < a[n-1]*/;k++/*,pre = pk*/,pk *= p){
-                long long lx = pk / p;
-                long long rx = pk;
-
-                int l = find_left(lx);
-                if(l == -1){
-                    continue;
-                }
-                int r = find_right(rx);
-                if(r == -1){
-                    continue;
-                }
-                if(l > r){
-                    continue;
-                }
-                long long tmp;
-                if(l == 0) tmp = sum[r][k];
-                else tmp = sum[r][k] - sum[l-1][k];
-                eans = (eans + tmp%MOD)%MOD;
-                //printf("p = %d,k = %d,search (%lld,%lld]\n",p,k,lx,rx);
-                //printf("l = %d,r = %d,add %lld\n\n",l,r,tmp);
-                if(pk >= a[n-1]) break;
-            }
-            px[p] = eans;
-            ans = (ans + (long long)i*eans%MOD)%MOD;
-        }
-        printf("%lld\n",ans);
-    }
+    test();
     return 0;
 }
